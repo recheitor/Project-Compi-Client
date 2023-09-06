d3e486af9c4d71194342a958f254a151352f55cc
Alejandro <123700084+recheitor@users.noreply.github.com>
2023-09-06 18:33:39 +0200

    score utils

---
+++ src/utils/updateDetails.utils.js
@@ -0,0 +1,12 @@
+const updateHouseRoomsDetails = (houseRoomDetails) => {
+    let totalScore = 0
+    houseRoomDetails.rating.forEach(({ score }) => totalScore += score)
+    totalScore = totalScore / houseRoomDetails.rating.length
+    houseRoomDetails.totalScore = totalScore
+    const updateLocation = { lat: houseRoomDetails.location.coordinates[1], lng: houseRoomDetails.location.coordinates[0] }
+    houseRoomDetails.location.coordinates = updateLocation
+    return houseRoomDetails
+}
+
+
+export default updateHouseRoomsDetails
